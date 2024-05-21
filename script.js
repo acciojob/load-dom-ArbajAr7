8d7a06d7b1af23b8d15b6fdb2fc5b35a4ce4aeae
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-21 23:41:31 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    var p = document.createElement('p');
+    p.textContent = 'DOM load success';
+	document.querySelector('body').appendChild(p);
+});
